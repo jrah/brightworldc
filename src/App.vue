292587a743f0d23beaea6b3022dbd3fa175ad3e9
James Home <james@jameshome.co.uk>
2020-09-22 00:34:51 +0100

    print out

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
+    <pre>
+      oldArray: {{oldArr.length}}
+      newArr (modified files): {{newArr.length}}
+      Final Output: {{finalOutput.length}}
+      Difference {{finalOutput.length - oldArr.length}}
+
+    </pre>
     <vue-blob-json-csv
       tag-name="div"
-      file-type="csv"
+      file-type="json"
       file-name="output"
       title="Download JSON"
       :data="merge(oldArr, newArr)"
       confirm="Do you want to download it?"
     />
+    <pre v-for="(item, i) in finalOutput" :key="i">
+      {{item.type}}
+      {{item.uid}}
+      {{item.meta_title}}
+    </pre>
   </div>
 </template>
 
 <script>
+import finalOutput from "./output.json";
+
 import oldArr from "./input/old.json";
 import newArr from "./input/new.json";
 
@@ -22,6 +36,7 @@ export default {
       obj: {},
       oldArr: oldArr,
       newArr: newArr,
+      finalOutput: finalOutput,
     };
   },
   components: {
@@ -38,7 +53,7 @@ export default {
       return setToArray;
     },
     merge(oldArr, newArr) {
-      const mergedArr = [...oldArr, ...newArr];
+      const mergedArr = [...newArr, ...oldArr];
       let set = new Set();
       let unionArr = mergedArr.filter((item) => {
         if (!set.has(item.uid)) {
@@ -52,14 +67,3 @@ export default {
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
