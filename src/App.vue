49bffcb22209ae33a71e9456f399f7407f07dc9b
James Home <james@jameshome.co.uk>
2020-09-22 01:07:20 +0100

    Add arraty compare

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,19 @@
       Final Output: {{finalOutput.length}}
       Difference {{finalOutput.length - oldArr.length}}
 
+      simpleMerge: {{simpleMerge(oldArr, newArr).length}}
+      Merge: {{merge(oldArr, newArr).length}}
+      Compare:{{ compareArray(merge(oldArr, newArr), oldArr ).length}}
+
+    <pre style="color: red;" v-for="(item, j) in compareArray(merge(oldArr, newArr), oldArr )" :key="j">
+      {{item.type}}
+      {{item.uid}}
+      {{item.meta_title}}
     </pre>
-    <pre v-for="(item, i) in finalOutput" :key="i">
+            
+            
+    </pre>
+    <pre v-for="(item, i) in simpleMerge(oldArr, newArr)" :key="i">
       {{item.type}}
       {{item.uid}}
       {{item.meta_title}}
@@ -27,7 +38,6 @@
 
 <script>
 import finalOutput from "./output.json";
-
 import oldArr from "./input/old.json";
 import newArr from "./input/new.json";
 
@@ -47,7 +57,7 @@ export default {
   },
   methods: {
     simpleMerge(oldArr, newArr) {
-      const AandB = [...oldArr, ...newArr];
+      const AandB = [...newArr, ...oldArr];
       console.log(AandB); // [ 44, 33, 22, 33, 22, 11 ]
       const distinctValuesInAandBSet = new Set(AandB);
       console.log(distinctValuesInAandBSet); // Set { 44, 33, 22, 11 }
@@ -66,37 +76,41 @@ export default {
       }, set);
       return unionArr;
     },
+    compareArray(arr1, arr2) {
+      const difference = arr1.filter(item1 => !arr2.some(item2 => (item2.meta_title === item1.meta_title)))
+      return difference;
+    },
   },
 };
 </script>
 
 <style lang="css">
 .convert {
-background-color: #fff;
-font-family: Arial, Helvetica, sans-serif;
-    border-color: #dbdbdb;
-    border-width: 1px;
-    color: #363636;
-    cursor: pointer;
-    justify-content: center;
-    padding: calc(.5em - 1px) 1em;
-    text-align: center;
-    white-space: nowrap;
-    background-color: #7957d5;
-    border-color: transparent;
-    color: #fff;
-    -webkit-appearance: none;
-    align-items: center;
-    border: 1px solid transparent;
-    border-radius: 4px;
-    box-shadow: none;
-    display: inline-flex;
-    font-size: 1rem;
-    height: 2.5em;
-    justify-content: flex-start;
-    line-height: 1.5;
-    padding: calc(.5em - 1px) calc(.75em - 1px);
-    position: relative;
-    vertical-align: top;
+  background-color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+  border-color: #dbdbdb;
+  border-width: 1px;
+  color: #363636;
+  cursor: pointer;
+  justify-content: center;
+  padding: calc(0.5em - 1px) 1em;
+  text-align: center;
+  white-space: nowrap;
+  background-color: #7957d5;
+  border-color: transparent;
+  color: #fff;
+  -webkit-appearance: none;
+  align-items: center;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  box-shadow: none;
+  display: inline-flex;
+  font-size: 1rem;
+  height: 2.5em;
+  justify-content: flex-start;
+  line-height: 1.5;
+  padding: calc(0.5em - 1px) calc(0.75em - 1px);
+  position: relative;
+  vertical-align: top;
 }
 </style>
